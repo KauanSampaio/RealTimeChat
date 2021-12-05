fb43c2737feb1e7df2c81322118af64f1e304aba
Kauan Sampaio <kauansampaio53@gmail.com>
2021-12-05 13:47:55 -0300

    Comments update

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+
 app.use('/', (req, res) =>{
     res.render('index.html');
 });
 
 let messages = [];
 
+// Socket.io communication listener
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
 
